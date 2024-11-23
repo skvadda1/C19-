c4fbb4b3f34163ea7895f5facf43f3459702f9f3
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-11-23 10:00:46 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#first function
+def fun1(n):
+    return n*(n+1)/2
+print(fun1(4))
+#second function
+def fun2(n):
+    sum = 0
+    for i in range(1, i+1):
+        sum += i
+#third function
+def fun3(n):
+    sum  = 0
+    for i in range(1, i+1):
+        for j in  range(i, i + 1):
+            sum += 1
+        return sum
+
